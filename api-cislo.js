01ceadf73d3e409a046a29bab37f4f6e5d5b73c0
Robin Martinu <martinu@porg.cz>
2019-11-11 17:39:08 +0100

    Finished basic template of the game

--- api-cislo.js
+++ api-cislo.js
@@ -1,4 +1,6 @@
 const url = require('url');
+let guessedNumber = "";
+
 
 function nahodneCisloOdDo(minimum, maximum) {
     return minimum + Math.trunc((maximum-minimum+1)*Math.random());
@@ -14,18 +16,51 @@ exports.apiCislo = function(req, res){
 
         let number = nahodneCisloOdDo(1, 4);
 
+
+        guessedNumber= number;
+
         obj.guessed = number;
+        obj.message = "";
+
+        if (obj.guessed == 1){
+            obj.message = "One";
+        } else if (obj.guessed == 2){
+            obj.message = "Two";
+        } else if (obj.guessed == 3) {
+            obj.message = "Three";
+        } else if (obj.guessed == 4) {
+            obj.message = "Four";
+        }
+/*
+        switch (number) {
+            case 1:
+                obj.message = "One";
+                break;
+            case 2:
+                obj.message = "Two";
+                break;
+            case 3:
+                obj.message = "Three";
+                break;
+            case 4:
+                obj.message = "Four";
+                break;
+        }
+*/
+
         res.end(JSON.stringify(obj));
 
-    } else if (q.pathname == "/cislo/inputnumber"){
+    } else if (q.pathname == "/cislo/guessnumber"){
         res.writeHead(200, {"Content-type": "application/json", "Access-Control-Allow-Origin":"*"});
         let obj = {};
-
-        let number = nahodneCisloOdDo(1, 4);
-
-        obj.guessed = number;
+        let q = url.parse(req.url, true);
+        let isGuessed = false;
+            if (q.query["guess"] && (q.query["guess"] == guessedNumber)){
+                isGuessed = true;
+            }
+        obj.isRight = isGuessed;
         res.end(JSON.stringify(obj));
 
     }
 
-}
+};
--- homepage.js
+++ homepage.js
@@ -1,16 +1,51 @@
 const HOST = window.location.protocol + "//" + window.location.hostname + ((window.location.port) ? ":" + window.location.port : "");
-
+let numbersRight = 0;
 function load (){
     fetch(HOST + "/cislo/newguess").then(function(response) {
         return response.text().then(function(text) {
             let obj = JSON.parse(text);
 
-            let number = obj.guessed;
-
-
-
-            alert (number);
+            alert (obj.message);
 
         });
     });
 }
+
+function checkGuess(guess) {
+
+    let url = HOST + "/cislo/guessnumber?guess=" + guess;
+
+     fetch(url).then(function(response) {
+        return response.text().then(function(text) {
+            let obj = JSON.parse(text);
+
+            if (obj.isRight) {
+                    document.getElementById("message").value += guess;
+                    numbersRight++;
+                    if (numbersRight == 4){
+                        numbersRight = 0;
+                        win();
+                    } else {
+                        load();
+                    }
+            }
+
+        });
+    });
+}
+
+function win (){
+    alert("Blahopřejeme, vyhráli jste!");
+    setTimeout(newGame,2000);
+
+}
+
+function newGame (){
+    document.getElementById("message").value = "";
+    load();
+}
+
+
+
+
+
