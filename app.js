ad3137849e3fdb70ea3d04872e2e7cfc8419c5bd
Robin Martinu <martinu@porg.cz>
2019-11-25 16:28:42 +0100

    Finished basic template of the game

--- app.js
+++ app.js
@@ -65,4 +65,4 @@ http.createServer((req, res) => {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset='UTF8'></head><body> Nothing here</body></html>");
     }
-}).listen(8888);
+}).listen(8080);
